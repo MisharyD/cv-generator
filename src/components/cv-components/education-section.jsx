6a822287bbb4d44137847673adbd0d42ccaf4b49
thord <masharyahmed123@gmail.com>
2024-09-17 17:57:44 +0300

    finish project cv section

--- src/components/cv-components/education-section.jsx
+++ src/components/cv-components/education-section.jsx
@@ -3,7 +3,7 @@ export default function EducationSection({ educationInfo }) {
     <div className="education-section">
       <h2 className=" mb-[8px] text-[24px] border-b-[1px] border-t-[0px] border-r-[0px] border-l-[0px] border-solid border-[#434242]">Education:</h2>
       {educationInfo.map((section) => (
-        <div key={section.id} className="education-section flex flex-col mb-[16px] last:mb-[0px]">
+        <div key={section.id} className="education-item flex flex-col mb-[16px] last:mb-[0px]">
           <div className="institutuion-date-container flex justify-between">
             <div className="font-bold text-[18px]">{section.institution}</div>
             <div>{section.date}</div>
--- src/components/cv-components/projects-section.jsx
+++ src/components/cv-components/projects-section.jsx
@@ -1,13 +1,17 @@
 export default function Projects({ projectInfo }) {
   return (
     <div className="project-section">
-      <h2>Projects:</h2>
+      <h2 className=" mb-[8px] text-[24px] border-b-[1px] border-t-[0px] border-r-[0px] 
+        border-l-[0px] border-solid border-[#434242]" >
+        Projects:
+      </h2>
       {projectInfo.map((section) => (
-        <div key={section.id}>
-          Project name:{section.name}
-          <br />
-          date: {section.date} <br />
-          about: {section.description} <br />
+        <div className = "project-item flex flex-col mb-[16px] last:mb-[0px]" key={section.id}>
+          <div className="name-date-container flex justify-between">
+            <div className="font-bold text-[18px]">{section.name}</div>
+            <div>{section.date}</div>
+          </div>
+          <div className ="whitespace-pre-wrap ml-[8px]">{section.description}</div>
         </div>
       ))}
     </div>
